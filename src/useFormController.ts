3d48cf12160e851a304368997f260b91e3fd8199
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-11-02 12:23:48 +0100

    Fix useLayoutEffect

--- src/useFormController.ts
+++ src/useFormController.ts
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState, useEffect } from 'react';
 import { FormController } from './internal/FormController';
 import { FieldAny, OnSubmit } from './internal/types';
 
@@ -7,13 +7,17 @@ export type UseFormControllerOptions<T extends FieldAny> = {
   onSubmit?: OnSubmit<T>;
 };
 
+declare const window: any;
+
+const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+
 export function useFormController<T extends FieldAny>({
   initialFields,
   onSubmit,
 }: UseFormControllerOptions<T>): FormController<T> {
   const [controller] = useState(() => new FormController({ initialFields, onSubmit }));
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     if (onSubmit) {
       controller.setOnSubmit(onSubmit);
     }
