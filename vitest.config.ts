97c2b3fba58eff4452b09b73b42aa3c52e5ceae1
Etienne Deladonchamps <etienne@husprey.com>
2023-06-27 13:51:30 +0200

    Fix setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
+  test: { environment: 'jsdom' },
 });
